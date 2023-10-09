7bd4a0eeac7e9f6fdaaf70548dd5cbf775a58bf7
Sona Shrivastava <134921666+sonagith@users.noreply.github.com>
2023-10-09 17:56:50 +0530

    Add files via upload

---
+++ assignmentoutput1.cpp
@@ -0,0 +1,94 @@
+//Qno.1
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+    //int x ;
+    //cout<<"enter first number\n";
+    //cin>>x;  //user will give'x' a value
+    //int y,m;
+    //cout<<"enter second number and value for taking modulus\n";
+    //cin>>y>>m; //user will give 'y' a value
+    //int Z = (x*y)%m;
+    //cout<<"Output is :"<<Z;
+//}
+//QNo.2
+//#include<iostream>
+//using  namespace std;
+//int main(){
+   // int x ;
+    //cout<<"Enter first number\n";
+    //cin>>x;
+    //int y;
+    //cout<<"Enter second number\n";
+    //cin>>y;
+    //cout<<(x!=y)<<" "<<(x>=y);
+//}
+
+//QNo.3
+//#include<iostream>
+//using namespace std;
+//int main(){
+    //int x,y;
+    //cin>>x>>y;
+    //x+=y;
+    //x-=y;
+    //x%=y;
+   // cout<<x;
+
+//}
+
+
+
+//QNo.4
+
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+    //float r;
+    //cin>>r;
+   // float h;
+    //cin>>h;
+    //float volume = 3.14*r*r*h;
+   // cout<<volume;
+//}
+
+
+
+
+//QNo.5
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+    //char c;
+    //cout<<"Enter a character:";
+   // cin>>c;
+   // cout<<" ASCII Value of "<<c<<" is "<<int(c);
+    //return 0 ;
+//}
+
+
+
+
+
+
+
+//QNo.6
+
+
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+    //int i = (4+7/5*6*6+9)%100;
+    //cout<<i;
+
+//}
+
+
+
+
+
+
